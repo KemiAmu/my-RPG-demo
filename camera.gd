b7030a5036bfbdfac8afe41486f72004a5d37580
Kemi-Amu <cmyist@outlook.com>
2025-04-30 13:43:45 +0800

    Update camera.gd

--- camera.gd
+++ camera.gd
@@ -10,13 +10,9 @@
 extends Camera2D
 
 @export var TARGET_VIEW_SIZE: Vector2 = Vector2(240, 160)
+@export var MOVE_DAMPING: float = -6.0
 @export var base_zoom: float = 1.0
-
-func _ready() -> void:
-	# change the camera zoom if window resized
-	var window: Window = get_window()
-	window.size_changed.connect(_on_window_resized.bind(window))
-	_on_window_resized(window)
+@export var target_position: Vector2 = Vector2(0, 0)
 
 # resize the camera when window resized
 func _on_window_resized(window: Window) -> void:
@@ -24,3 +20,17 @@ func _on_window_resized(window: Window) -> void:
 	var view_zoom: float = max(view_scale.x, view_scale.y) * base_zoom
 	self.zoom = Vector2(view_zoom, view_zoom)
 	print("camera zoomed: ", view_zoom)
+
+# set the target position of the camera
+func set_target_position(new_position: Vector2) -> void:
+	target_position = new_position
+
+func _ready() -> void:
+	# change the camera zoom if window resized
+	var window: Window = get_window()
+	window.size_changed.connect(_on_window_resized.bind(window))
+	_on_window_resized(window)
+	
+func _process(delta: float) -> void:
+		# update the camera position
+		position = position.lerp(target_position, 1 - exp(MOVE_DAMPING * delta))
