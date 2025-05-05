adcc81e96f0a2b910f431cf670fb06816fc32a50
Kemi-Amu <KemiAmu@yeah.net>
2025-05-05 14:05:20 +0800

    Update save_manager.gd

--- save/save_manager.gd
+++ save/save_manager.gd
@@ -7,30 +7,42 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
+# 存档管理器，满足全局动态加载/保存
+
 class_name SaveManager
 extends Resource
 
 const SAVE_PATH := "user://auto_save.data"
 
 var registered_objects := {}
+var loaded_data := {}
+
+func register(key: String, obj: Object) -> void:
+	registered_objects[key] = obj
+
+	if loaded_data.has(key) and obj.has_method("load"):
+		obj.call("load", loaded_data[key])
 
 func save() -> void:
 	var data := {}
 	for key in registered_objects:
-		data[key] = registered_objects[key].save()
+		if registered_objects[key].has_method("save"):
+			data[key] = registered_objects[key].call("save")
 
 	var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-	file.store_var(data)
-	file.close()
+	if file:
+		file.store_var(data)
+		file.close()
 
 func load() -> void:
 	if not FileAccess.file_exists(SAVE_PATH):
 		return
 
 	var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
-	var data = file.get_var()
-	file.close()
+	if file:
+		loaded_data = file.get_var()
+		file.close()
 
-	for key in data:
-		if key in registered_objects:
-			registered_objects[key].load(data[key])
+	for key in registered_objects:
+		if loaded_data.has(key) and registered_objects[key].has_method("load"):
+			registered_objects[key].call("load", loaded_data[key])
