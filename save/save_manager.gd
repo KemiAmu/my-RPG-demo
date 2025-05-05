618dfbf17424bd53a4fe923c6e37be283495687c
Kemi-Amu <KemiAmu@yeah.net>
2025-05-05 14:16:23 +0800

    Update save_manager.gd

--- save/save_manager.gd
+++ save/save_manager.gd
@@ -7,47 +7,54 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-# 存档管理器，满足全局动态加载/保存
-
+# 存档管理器单例，满足全局动态加载和保存
 class_name SaveManager
 extends Resource
 
+# Path to the save file
 const SAVE_PATH := "user://auto_save.data"
 
+# Dictionary to store save callbacks
 var save_funcs := {}
+# Dictionary to store load callbacks
 var load_funcs := {}
+# Dictionary to store loaded data
 var loaded_data := {}
 
+# Register a new save/load handler for a given key
 func register(key: String, save_cb: Callable, load_cb: Callable) -> void:
 	save_funcs[key] = save_cb
 	load_funcs[key] = load_cb
-	
+
 	if loaded_data.has(key):
 		load_cb.call(loaded_data[key])
 
+# Unregister a save/load handler by key
 func unregister(key: String) -> void:
 	save_funcs.erase(key)
 	load_funcs.erase(key)
 
+# Save all registered data to file
 func save() -> void:
 	var data := {}
 	for key in save_funcs:
 		data[key] = save_funcs[key].call()
-	
+
 	var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
 	if file:
 		file.store_var(data)
 		file.close()
 
+# Load all registered data from file
 func load() -> void:
 	if not FileAccess.file_exists(SAVE_PATH):
 		return
-	
+
 	var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
 	if file:
 		loaded_data = file.get_var()
 		file.close()
-	
+
 	for key in load_funcs:
 		if loaded_data.has(key):
 			load_funcs[key].call(loaded_data[key])
