4575b6c76c8f7222ee3f59821e8f30a90ce67a42
Kemi-Amu <KemiAmu@yeah.net>
2025-05-05 14:10:12 +0800

    Update save_manager.gd

--- save/save_manager.gd
+++ save/save_manager.gd
@@ -9,7 +9,6 @@
 
 # 存档管理器，满足全局动态加载/保存
 
-
 class_name SaveManager
 extends Resource
 
@@ -20,31 +19,31 @@ var load_funcs := {}
 var loaded_data := {}
 
 func register(key: String, save_cb: Callable, load_cb: Callable) -> void:
-    save_funcs[key] = save_cb
-    load_funcs[key] = load_cb
-    
-    if loaded_data.has(key):
-        load_cb.call(loaded_data[key])
+	save_funcs[key] = save_cb
+	load_funcs[key] = load_cb
+	
+	if loaded_data.has(key):
+		load_cb.call(loaded_data[key])
 
 func save() -> void:
-    var data := {}
-    for key in save_funcs:
-        data[key] = save_funcs[key].call()
-    
-    var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-    if file:
-        file.store_var(data)
-        file.close()
+	var data := {}
+	for key in save_funcs:
+		data[key] = save_funcs[key].call()
+	
+	var file := FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if file:
+		file.store_var(data)
+		file.close()
 
 func load() -> void:
-    if not FileAccess.file_exists(SAVE_PATH):
-        return
-    
-    var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
-    if file:
-        loaded_data = file.get_var()
-        file.close()
-    
-    for key in load_funcs:
-        if loaded_data.has(key):
-            load_funcs[key].call(loaded_data[key])
+	if not FileAccess.file_exists(SAVE_PATH):
+		return
+	
+	var file := FileAccess.open(SAVE_PATH, FileAccess.READ)
+	if file:
+		loaded_data = file.get_var()
+		file.close()
+	
+	for key in load_funcs:
+		if loaded_data.has(key):
+			load_funcs[key].call(loaded_data[key])
