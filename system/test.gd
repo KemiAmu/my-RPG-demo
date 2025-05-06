9a7049b82db3307c32a76e225e94ec73595ca10d
Kemi-Amu <KemiAmu@yeah.net>
2025-05-06 14:04:02 +0800

    u.

--- system/test.gd
+++ system/test.gd
@@ -11,10 +11,12 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	print("Hello, World!")
 	Dialogic.start("timeline")
 	
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+@warning_ignore("unused_parameter")
 func _process(delta: float) -> void:
 	pass
